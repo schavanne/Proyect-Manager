72325824b81a29e130bf146919dc126403d41c7b
schavanne <sir_sirchu@hotmail.com>
2023-01-31 22:52:13 -0300

    se realizo login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const User = require('../database/models/User');
 const errorResponse = require('../helpers/errorResponse');
+const generateJWT = require('../helpers/generateJWT');
 const generateTokenRandom = require('../helpers/generateTokenRandom');
 
 module.exports = {
@@ -44,17 +45,51 @@ module.exports = {
 },
 login : async (req,res) => {
 
+    const {email,password} = req.body;
+
+
+
     try{ 
+        if([email,password].includes("")){
+            throw createError(400,"Todos los campos son obligatorios");
+        
+        };
+
+        /*let user = await User.findOne({
+            email
+        }, (error, user)=>{
+            if(error) return handleError(error)
+        }); */
+
+        let user = await User.findOne({
+            email
+        })
+
+        if(!user){
+            throw createError(403,"Credenciales invalidas | email");
+        };
+
+        if(!user.checked){
+            throw createError(403,"Tu cuenta no ha sido confirmada");
+        };
+
+        if(!await user.checkedPassword(password)){
+            throw createError(403,"Credenciales invalidas | password");
+        }
+
         return res.status(200).json({
             ok : true,
-            msg : 'Usuario Logeado'
+            msg : 'Usuario Logeado',
+            user : {
+                nombre : user.name,
+                email : user.email,
+                token : generateJWT({
+                    id: user._id
+            })
+            }
         })
     } catch (error) {
-        console.log(error);
-        return res.status(error.status || 500).json({
-            ok : false,
-            msg : error.message || 'Upss, hubo un error en el login'
-        })
+        return errorResponse(res,error, "login")
     }
 },
 checked : async (req,res) => {
